63fbd08210953347a1881a030dbe4c35f3eb976e
automationwork <shashikantm@gmail.com>
2016-10-04 15:33:06 +0530

    new update
    workspace set up for the new automation tool.

---
+++ Workspace/Timer/src/com/to/User.java
@@ -0,0 +1,26 @@
+package com.to;
+
+public class User {
+
+	private String loginUserName;
+	
+	private String loginPassWord;
+
+	public String getLoginUserName() {
+		return loginUserName;
+	}
+
+	public void setLoginUserName(String loginUserName) {
+		this.loginUserName = loginUserName;
+	}
+
+	public String getLoginPassWord() {
+		return loginPassWord;
+	}
+
+	public void setLoginPassWord(String loginPassWord) {
+		this.loginPassWord = loginPassWord;
+	}
+	
+	
+}
